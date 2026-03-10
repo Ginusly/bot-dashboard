7ea6504ccd3cf3fc7049420d60c8bbcf73558213
Ginusly <scanfiyai@gmail.com>
2026-03-10 14:44:44 +0200

    Temporarily disable Firebase to test build

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,9 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, doc, onSnapshot, getDoc, query, orderBy, limit } from 'firebase/firestore';
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCGhsF-nlCsK2FFtcxccUdlezUSOxBVnxU",
-    authDomain: "umbralbot-734bd.firebaseapp.com",
-    projectId: "umbralbot-734bd",
-    storageBucket: "umbralbot-734bd.firebasestorage.app",
-    messagingSenderId: "303425465602",
-    appId: "1:303425465602:web:d4bbdd818e14e60c87661b",
-    measurementId: "G-KSNV16NTFR"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db, collection, doc, onSnapshot, getDoc, query, orderBy, limit };
+// Temporary firebase.js for build testing
+export const db = null;
+export const collection = null;
+export const doc = null;
+export const onSnapshot = null;
+export const getDoc = null;
+export const query = null;
+export const orderBy = null;
+export const limit = null;
