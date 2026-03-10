d2420795e9f359fcbd244f2e9b1a9d76a44ab2ea
Ginusly <scanfiyai@gmail.com>
2026-03-10 15:30:46 +0200

    Restore Firebase for full functionality

---
+++ client/src/firebase-temp.js
@@ -0,0 +1,9 @@
+// Temporary firebase.js for build testing
+export const db = null;
+export const collection = null;
+export const doc = null;
+export const onSnapshot = null;
+export const getDoc = null;
+export const query = null;
+export const orderBy = null;
+export const limit = null;
